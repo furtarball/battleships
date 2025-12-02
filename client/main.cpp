4c377d6cef553f744c1fc593b5fd79b54771dd4f
furtarball <61846505+furtarball@users.noreply.github.com>
2025-12-03 00:40:24 +0100

    Report runtime exceptions in a window

--- client/main.cpp
+++ client/main.cpp
@@ -1,8 +1,15 @@
+#include <SDL3/SDL.h>
+#include <stdexcept>
 #include <string>
 import battleships_client;
 
 int main(int argc, char** argv) {
-	Client client{argv[1], std::stoi(argv[2])};
-	while (client.run() > 0)
-		;
+	try {
+		Client client{argv[1], std::stoi(argv[2])};
+		while (client.run() > 0)
+			;
+	} catch (std::runtime_error& e) {
+		SDL_ShowSimpleMessageBox(
+			SDL_MESSAGEBOX_ERROR, "Battleships", e.what(), nullptr);
+	}
 }
